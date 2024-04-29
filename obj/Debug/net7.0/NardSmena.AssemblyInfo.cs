6e6ff5d4d5e88a02c027fa8bbf3a6751486a9fa8
Ilya <dobrixgames@gmail.com>
2024-04-29 16:51:58 +0300

    Updated GUI and fixed data in table (SprOperation), addedd alert in page SprOperation

--- obj/Debug/net7.0/NardSmena.AssemblyInfo.cs
+++ obj/Debug/net7.0/NardSmena.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NardSmena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5dfe3c447c4bd4aeeb96fb3d37fe9ca1e0e2461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9eed5582b85944ae9c7e15dd09bd65f47ab0ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("NardSmena")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NardSmena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
